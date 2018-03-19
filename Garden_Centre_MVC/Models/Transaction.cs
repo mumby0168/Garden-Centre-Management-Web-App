4d32b2c4d6f2e87eafefafb84b8c556df177fe87
Robert Bennett <robertbennett1998@outlook.com>
2018-03-19 09:57:19 +0000

    Added transaction model

---
+++ Garden_Centre_MVC/Models/Transaction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Garden_Centre_MVC.Models;
+
+namespace Garden_Centre_MVC.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+
+        public int TransactionNumber { get; set; }
+
+        public int ItemId { get; set; }
+
+        public Item Item { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public Customer Customer { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        public float Value { get; set; }
+    }
+}
