c07be4bd965746cf8b219269e1110dc5362e5d55
Robert Bennett <robertbennett1998@outlook.com>
2018-03-19 11:19:05 +0000

    Added stuff

---
+++ Garden_Centre_MVC/Controllers/TransactionsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Garden_Centre_MVC.Controllers
+{
+    public class TransactionsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
---
+++ Garden_Centre_MVC/ViewModels/TransactionsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garden_Centre_MVC.ViewModels
+{
+    public class TransactionsViewModel
+    {
+    }
+}
