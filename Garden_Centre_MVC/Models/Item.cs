e0aa279850cbf6ad35b8da0c3f4bc451707463c3
Robert Bennett <robertbennett1998@outlook.com>
2018-03-19 10:07:53 +0000

    Add item model

---
+++ Garden_Centre_MVC/Models/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garden_Centre_MVC.Models
+{
+    public class Item
+    {
+        public int ItemId { get; set; }
+
+        public float ItemPrice { get; set; }
+
+        public int Stock { get; set; }
+
+        public int OnOrder { get; set; }
+
+        public int Sold { get; set; }
+    }
+}
