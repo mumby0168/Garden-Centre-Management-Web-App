0dec9a26164dbecb73a4e54bafe44e434cc8d1ed
Robert Bennett <robertbennett1998@outlook.com>
2018-04-18 11:37:34 +0100

    Started on the Inventory view, need to discuss some details in next meeting, it actually is a fairly simple screen

---
+++ Garden_Centre_MVC/Controllers/InventoryController.cs
@@ -0,0 +1,45 @@
+﻿using Garden_Centre_MVC.Models;
+using Garden_Centre_MVC.Persistance;
+using Garden_Centre_MVC.ViewModels.InventoryViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Garden_Centre_MVC.Controllers
+{
+    public class InventoryController : Controller, IDisposable
+    {
+        private DatabaseContext m_Context = new DatabaseContext();
+        public void Dispose()
+        {
+            m_Context.Dispose();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult InventoryView()
+        {
+            InventoryViewModel vm = new InventoryViewModel();
+            return PartialView("Partials/InventoryView", vm);
+        }
+
+        public PartialViewResult NewItemView()
+        {
+            InventoryViewModel vm = new InventoryViewModel();
+            return PartialView("Partials/NewItemView");
+        }
+
+        public PartialViewResult NewItemSerialize(Item newItem)
+        {
+            m_Context.Items.Add(newItem);
+            m_Context.SaveChanges();
+
+            return InventoryView();
+        }
+    }
+}
---
+++ Garden_Centre_MVC/ViewModels/InventoryViewModels/InventoryViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Garden_Centre_MVC.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Garden_Centre_MVC.Models;
+
+namespace Garden_Centre_MVC.ViewModels.InventoryViewModels
+{
+    public class InventoryViewModel : IDisposable
+    {
+        private DatabaseContext m_Context = new DatabaseContext();
+        public void Dispose()
+        {
+            m_Context.Dispose();
+        }
+
+        public List<Item> inventory
+        {
+            get
+            {
+                return m_Context.Items.ToList();
+            }
+        }
+    }
+}
