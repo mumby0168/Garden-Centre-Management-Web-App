ea6107510726c300bf9511e5853c6ffa805fb9ef
Robert Bennett <robertbennett1998@outlook.com>
2018-04-20 19:21:22 +0100

    Added to paging

--- Garden_Centre_MVC/Scripts/Paging.js
+++ Garden_Centre_MVC/Scripts/Paging.js
@@ -1,21 +1,44 @@
 class Paging {
-    constructor(data, tableId, headers, rowLambda, blankRows, btnClass) {
+    constructor(data, tableId, headers, rowLambda, btnClass) {
         this.m_Data = data;
+        this.m_SearchData = data;
         this.m_Table = document.getElementById(tableId);
         this.m_Headers = headers;
         this.m_RowLambda = rowLambda;
         this.m_BtnClass = btnClass;
         this.m_RowsToDisplay = 10;
-        this.m_DispBlankRows = blankRows;
 
         this.CreateTableHeader();
+    }
+
+    Search(TextBoxId, FieldToSearch) {
+        var tb = document.getElementById(TextBoxId);
+
+        this.SearchResults = new Array();
+
+        for (var i = 0; i < this.m_Data.length; i++) {
+            if (this.m_Data[i][FieldToSearch].toUpperCase().includes(tb.value.toUpperCase())) {
+                this.SearchResults.push(this.m_Data[i]);
+            }
+        }
+
+        alert(this.SearchResults.length);
+
+        this.m_SearchData = this.SearchResults;
+
+        this.Page(1)
+
+    }
+
+    ClearSearch() {
+        this.m_SearchData = this.m_Data;
     }
 
     Page(page) {
         if (page < 1)
             return;
 
-        if (this.m_Data.length - ((page - 1) * this.m_RowsToDisplay) <= 0)
+        if (this.m_SearchData.length - ((page - 1) * this.m_RowsToDisplay) <= 0)
             return;
 
         this.m_Page = page;
@@ -24,11 +47,8 @@ class Paging {
         this.CreateTableHeader();
 
         for (var i = (this.m_Page - 1) * this.m_RowsToDisplay; i < this.m_Page * this.m_RowsToDisplay; i++) {
-            if (i < this.m_Data.length) {
-                this.m_RowLambda(this.m_Data[i], this.m_Table);
-            }
-            else if (this.m_DispBlankRows === undefined || this.m_DispBlankRows === false) {
-                break;
+            if (i < this.m_SearchData.length) {
+                this.m_RowLambda(this.m_SearchData[i], this.m_Table);
             }
             else {
                 var tr = document.createElement("tr");
@@ -43,7 +63,9 @@ class Paging {
         var tr = document.createElement("tr");
         var td = document.createElement("td");
         var div = document.createElement("div");
+        div.setAttribute("style", "vertical-align:middle;");
         div.setAttribute("align", "center");
+
         td.setAttribute("colspan", this.m_Headers.length);
 
         var btn = document.createElement("button");
@@ -59,9 +81,22 @@ class Paging {
 
         div.appendChild(btn);
 
+        btn = document.createElement("button");
+
+        if (this.m_BtnClass !== undefined)
+            btn.setAttribute("class", this.m_BtnClass);
+
+        btn.innerHTML = "Next";
+        btn.addEventListener("click", (e) => {
+            this.Page(this.m_Page + 1);
+        });
+        btn.setAttribute("style", "float:right;");
+
+        div.appendChild(btn);
+
         var sel = document.createElement("select");
 
-        //sel.setAttribute("style", "float:left;")
+        sel.setAttribute("style", "float:right; margin-right: 5px;")
 
         sel.addEventListener("change", (e) => {
             this.m_RowsToDisplay = parseInt(e.target.value);
@@ -104,20 +139,16 @@ class Paging {
         sel.appendChild(opt);
         sel.value = this.m_RowsToDisplay;
 
+        var numPages = parseInt(this.m_SearchData.length / this.m_RowsToDisplay);
+        if (this.m_SearchData.length % this.m_RowsToDisplay !== 0)
+            numPages += 1;
+
         div.appendChild(sel);
 
-        btn = document.createElement("button");
+        var p = document.createElement("p");
+        p.innerHTML = "Page " + this.m_Page + " of " + numPages;
 
-        if (this.m_BtnClass !== undefined)
-            btn.setAttribute("class", this.m_BtnClass);
-
-        btn.innerHTML = "Next";
-        btn.addEventListener("click", (e) => {
-            this.Page(this.m_Page + 1);
-        });
-        btn.setAttribute("style", "float:right;");
-
-        div.appendChild(btn);
+        div.appendChild(p);
         td.appendChild(div);
         tr.appendChild(td);
         this.m_Table.appendChild(tr);
