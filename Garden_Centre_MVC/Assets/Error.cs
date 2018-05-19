f42244767e596e2db17b167f0b4bf5ea58d951f9
billy mumby <billy.mumby@outlook.com>
2018-05-19 16:03:09 +0100

    Commented the assets folder.

--- Garden_Centre_MVC/Assets/Error.cs
+++ Garden_Centre_MVC/Assets/Error.cs
@@ -5,6 +5,10 @@ using System.Web;
 
 namespace Garden_Centre_MVC.Assets
 {
+    /// <summary>
+    /// this object is used in order to create the list off errors that are used in the
+    /// customer and employee pages.
+    /// </summary>
     public class Error
     {
         public object Property { get; set; }
