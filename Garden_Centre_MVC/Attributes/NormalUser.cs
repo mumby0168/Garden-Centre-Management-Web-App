1e76ca52b1d103209e6067b977951c9383782703
billy mumby <billy.mumby@outlook.com>
2018-05-21 07:28:52 +0100

    commented all of the attributes.

--- Garden_Centre_MVC/Attributes/NormalUser.cs
+++ Garden_Centre_MVC/Attributes/NormalUser.cs
@@ -9,8 +9,17 @@ using Garden_Centre_MVC.Attributes.Assets;
 
 namespace Garden_Centre_MVC.Attributes
 {
+    /// <summary>
+    /// This again is a attribute that will asses when a request comes into a controller or a method whether the user
+    /// currently has a sessions running. If they do not it will redirect them to another page.
+    /// </summary>
     public class NormalUser : AuthorizeAttribute
     {
+        /// <summary>
+        /// this method returns a boolean and will state if the user has a sessions running.
+        /// </summary>
+        /// <param name="httpContext"></param>
+        /// <returns></returns>
         protected override bool AuthorizeCore(HttpContextBase httpContext)
         {
             try
@@ -24,6 +33,11 @@ namespace Garden_Centre_MVC.Attributes
             
         }
 
+        /// <summary>
+        /// this will be called if the return from the above method is false and it will redirect the user to the
+        /// login screen.
+        /// </summary>
+        /// <param name="filterContext"></param>
         protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
         {
             filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new {controller = "Account", action = "Index"}));
