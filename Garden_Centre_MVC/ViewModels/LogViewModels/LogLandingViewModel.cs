15d9bf5f0012cfd086f6f599cfe3d194ed154bbd
billy mumby <billy.mumby@outlook.com>
2018-05-21 07:10:28 +0100

    commented the log, customer, and account viewmodels.

--- Garden_Centre_MVC/ViewModels/LogViewModels/LogLandingViewModel.cs
+++ Garden_Centre_MVC/ViewModels/LogViewModels/LogLandingViewModel.cs
@@ -6,6 +6,10 @@ using Garden_Centre_MVC.Models;
 
 namespace Garden_Centre_MVC.ViewModels.LogViewModels
 {
+    /// <summary>
+    /// This is a view model and it holds properties that can be set dynamically and this will relate to a view
+    /// The view will take these properties and then display them as we choose in the .cshtml files.
+    /// </summary>
     public class LogLandingViewModel
     {
         public List<Log> Logs { get; set; }
