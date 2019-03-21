7820cac9183c0909de9858be2dc58479bcb7f5c0
billy mumby <billy.mumby@outlook.com>
2019-03-21 12:42:59 +0000

    updated the packages to use a newer version of the rosyln compiler.

--- Garden_Centre_MVC/Models/Employee.cs
+++ Garden_Centre_MVC/Models/Employee.cs
@@ -22,7 +22,7 @@ namespace Garden_Centre_MVC.Models
 
         [Required]
         [MaxLength(15)]
-        [Display(Name = "Second Name")]Admin
+        [Display(Name = "Second Name")]
         public string SecondName { get; set; }
 
         [Required]
